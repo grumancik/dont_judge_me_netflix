29db1951d90e264206c9cf489e3b647a22920ab2
Gabriel Gauffin <gabriel.eriksson@hyperisland.se>
2016-02-21 16:47:15 +0100

    Started building the logic.

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
-if (){
+setInterval(function(){if (document.getElementsByClassName("button continue-playing")) {
+// Run the voice recognition script, and get return? Is this even close to right?-->$.getScript(speech_recognition.js, ){}
+}},1000);
+
+if (/*voice="stop judging me"*/){
   setInterval(function(){try{document.getElementsByClassName("button continue-playing")[0].click()}catch(e){}}, 33);
 }
-else{
+else if (/*voice="back to browse"*/) {
   setInterval(function(){try{document.getElementsByClassName("button back-to-browse")[0].click()}catch(e){}}, 33);
+}
+else {
+
 }
