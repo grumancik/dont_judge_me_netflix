acb3257f43de819b12c75eb1c29b4783571b4fdf
Gabriel Gauffin <gabriel.eriksson@hyperisland.se>
2016-02-21 15:41:49 +0100

    Adding files

---
+++ background.js
@@ -0,0 +1,6 @@
+if (){
+  setInterval(function(){try{document.getElementsByClassName("button continue-playing")[0].click()}catch(e){}}, 33);
+}
+else{
+  setInterval(function(){try{document.getElementsByClassName("button back-to-browse")[0].click()}catch(e){}}, 33);
+}
